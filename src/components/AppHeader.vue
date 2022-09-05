46a98cf2577966dc8eca58e5c59fd25cf0474513
Vladimir <86754494+dazeGG@users.noreply.github.com>
2022-09-05 13:49:13 +0300

    added header component

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,11 @@
+<template>
+  <header class="header">
+    <div class="container">
+      <AppHeaderAuth/>
+    </div>
+  </header>
+</template>
+
+<script setup lang="ts">
+import AppHeaderAuth from '@/components/AppHeaderAuth.vue'
+</script>
