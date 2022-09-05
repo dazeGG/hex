9b3cbf2359e70881a31913be14055500bef3bc7c
Vladimir <86754494+dazeGG@users.noreply.github.com>
2022-09-05 18:39:37 +0300

    added logout function

--- src/components/AppHeader/components/AppHeaderAuth.vue
+++ src/components/AppHeader/components/AppHeaderAuth.vue
@@ -1,12 +1,21 @@
 <template>
   <div class="header__wrapper">
-    <router-link to="/" class="header__logo">dazeGG</router-link>
-    <router-link to="/auth" class="header__login">Login</router-link>
+    <router-link :to="Routes.links" class="header__logo">dazeGG</router-link>
+    <button class="header__login button red" @click="logout">Logout</button>
   </div>
 </template>
 
 <script setup lang="ts">
-</script>
+import Routes from '@/router/Routes'
+import { useRouter } from 'vue-router'
+import { useStore } from 'vuex'
 
-<style scoped lang="scss">
-</style>
+const router = useRouter()
+const store = useStore()
+
+const logout = () => {
+  localStorage.removeItem('access_token')
+  store.commit('Auth/setIsAuth', false)
+  router.push({ path: Routes.home })
+}
+</script>
