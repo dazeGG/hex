08127f17ace5af2117dc1c342d862c3937466ecc
Vladimir <86754494+dazeGG@users.noreply.github.com>
2022-09-05 13:49:32 +0300

    added main layout

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="layout">
+    <AppHeader/>
+    <router-view/>
+  </div>
+</template>
+
+<script setup lang="ts">
+import AppHeader from '@/components/AppHeader.vue'
+</script>
+
+<style scoped lang="scss">
+.layout {
+  display: grid;
+  grid-template-rows: auto 1fr;
+}
+</style>
