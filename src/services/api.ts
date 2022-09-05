465373e35e70ed4d76cf90d3572a3ef26db7b357
Vladimir <86754494+dazeGG@users.noreply.github.com>
2022-09-05 19:24:34 +0300

    added access token to requests

--- src/services/api.ts
+++ src/services/api.ts
@@ -2,4 +2,15 @@ import axios from 'axios'
 
 const api = axios.create({ baseURL: 'http://79.143.31.216' })
 
+api.interceptors.request.use(
+  (config: any) => {
+    if (localStorage.access_token) {
+      config.headers.Authorization = `Bearer ${localStorage.access_token}`
+    } else {
+      config.headers = ''
+    }
+    return config
+  }
+)
+
 export default api
