c27df69dc70c2925d4f34feeb24d890399065b7a
Vladimir <86754494+dazeGG@users.noreply.github.com>
2022-09-05 19:24:57 +0300

    added work with links to store

---
+++ src/store/modules/Links.ts
@@ -0,0 +1,28 @@
+import { getStatistics } from '@/services/links.service'
+
+const Links = {
+  namespaced: true,
+  state: {
+    links: []
+  },
+  getters: {
+    links: (state: any) => state.links
+  },
+  mutations: {
+    setLinks (state: any, payload: any) {
+      state.links = payload
+    }
+  },
+  actions: {
+    async getStatistics ({ commit }: any, { order = 'asc_short', offset = 0, limit = 0 }) {
+      try {
+        const res = await getStatistics(order, offset, limit)
+        commit('setLinks', res.data)
+      } catch (e) {
+        console.log(e)
+      }
+    }
+  }
+}
+
+export default Links
