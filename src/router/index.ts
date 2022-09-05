6cc73afca7de437263e40e0ffef3a0d9660c5c70
Vladimir <86754494+dazeGG@users.noreply.github.com>
2022-09-05 18:41:29 +0300

    some routes fix

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,24 +1,42 @@
 import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router'
 import Routes from '@/router/Routes'
+import store from '@/store'
 
 const routes: Array<RouteRecordRaw> = [
   {
-    path: '/',
-    redirect: '/links'
-  },
-  {
     path: Routes.home,
     name: 'home',
+    meta: {
+      needAuth: false,
+      disabledToAuth: true
+    },
     component: () => import('../views/HomeView.vue')
+  },
+  {
+    path: Routes.links,
+    name: 'links',
+    meta: {
+      needAuth: true,
+      disabledToAuth: false
+    },
+    component: () => import('../views/LinksView.vue')
   },
   {
     path: Routes.registration,
     name: 'registration',
+    meta: {
+      needAuth: false,
+      disabledToAuth: true
+    },
     component: () => import('../views/RegistrationView.vue')
   },
   {
     path: Routes.authorization,
     name: 'authorization',
+    meta: {
+      needAuth: false,
+      disabledToAuth: true
+    },
     component: () => import('../views/AuthorizationView.vue')
   }
 ]
@@ -26,6 +44,22 @@ const routes: Array<RouteRecordRaw> = [
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
   routes
+})
+
+router.beforeEach(async (to, from, next) => {
+  await store.dispatch('Auth/initAuth', {}, { root: true })
+    .then(() => {
+      if (store.getters['Auth/isAuth']) {
+        if (to.meta.disabledToAuth) {
+          next({ path: Routes.links })
+        }
+        next()
+      } else {
+        if (to.meta.needAuth) {
+          next({ path: Routes.home })
+        } else next()
+      }
+    })
 })
 
 export default router
